d898d524727ffe8d49f869f4d98f773f32c0cdf7
Pathomkorn Muttarak <pathomkorn@gmail.com>
2018-06-29 11:40:15 +0700

    Update and rename va-pods-cannot-start.md to va-pods-cannot-start.sh

--- va-pods-cannot-start.sh
+++ va-pods-cannot-start.sh
@@ -1,3 +1,2 @@
-```bash
+#!/bin/bash
 kubectl patch statefulset/vulnerability-advisor-kafka -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"command":["bash","-c","KAFKA_BROKER_ID=$((${HOSTNAME##*-}+1001)) KAFKA_RESERVED_BROKER_MAX_ID=$((${HOSTNAME##*-}+1001)) /usr/bin/start-kafka.sh"],"env":[{"name":"KAFKA_DELETE_TOPIC_ENABLE","value":"true"}],"name":"kafka-server"}]}}}}'
-```
